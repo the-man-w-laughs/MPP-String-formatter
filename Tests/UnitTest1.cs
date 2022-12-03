72e399a289739c2a4e4dbe01f193015d13667475
Nazar <nazarmasukov@gmail.com>
2022-12-03 15:05:02 +0300

    Added new tests.

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,13 +1,30 @@
 namespace Tests;
 
 public class UnitTest1
-{
+{    
+    [Fact]
+    public void InvalidBracketSequence()
+    {
+        var user = new User("Nazar", "Masiukou");
+        Assert.Throws<InvalidTemplateException>(
+            () => StringFormatter.Shared.Format("{} {", user));
+        Assert.Throws<InvalidTemplateException>(
+            () => StringFormatter.Shared.Format("}{", user));
+        Assert.Throws<InvalidTemplateException>(
+            () => StringFormatter.Shared.Format("{} }", user));
+    }
+    [Fact]
+    public void EscapeCharacterTest(){
+        var user = new User("Nazar", "Masiukou");
+        Assert.Equal("{}",
+        StringFormatter.Shared.Format("{{}}", user));
+    }
+
     [Fact]
     public void SimpleStringFormatTest()
     {
-
-        var user = new User("Nazar", "Masiukou");    
+        var user = new User("Nazar", "Masiukou");
         Assert.Equal($"Ну привет {user.FirstName} {user.LastName}!",
-         StringFormatter.Shared.Format("Ну привет {FirstName} {LastName}!",user));
+        StringFormatter.Shared.Format("Ну привет {FirstName} {LastName}!", user));
     }
 }
